2788cfdaf7d662c8eec74d8468f894244fdbcf82
Ming <mwang@usc.edu.au>
2026-09-01 11:31:16 +1000

    Initial starter code

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
+    // id("org.jetbrains.kotlin.kapt") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "2.3.9" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
